9fe54e38215b6854b202c4cb5edcc3b417f9cfd5
ieajohsila <50976723+ieajohsila@users.noreply.github.com>
2025-12-18 23:48:26 +0330

    Refactor RSS sources and add default sources
    Replaced RSS_SOURCES with DEFAULT_RSS_SOURCES and DEFAULT_SCRAPE_SITES.

---
+++ default_sources.py
@@ -0,0 +1,18 @@
+DEFAULT_RSS_SOURCES = [
+    "https://www.hollywoodreporter.com/c/news/feed/",
+    "https://www.imdb.com/news/top/?rss",
+    "https://www.euronews.com/tag/cinema/rss",
+    "https://www.screendaily.com/rss",
+    "https://www.empireonline.com/movies/news/rss/",
+    "https://www.zoomg.ir/cinema/feed/",
+    "https://variety.com/v/film/feed/",
+    "https://variety.com/v/tv/feed/",
+    "https://deadline.com/feed/"
+]
+
+DEFAULT_SCRAPE_SITES = [
+    "https://www.hollywoodreporter.com/c/news/",
+    "https://www.imdb.com/news/top/",
+    "https://www.screendaily.com/news",
+    "https://www.empireonline.com/movies/news/"
+]
--- rss_sources.py
+++
@@ -1,7 +0,0 @@
-RSS_SOURCES = [
-    {"name": "Variety Film", "url": "https://variety.com/v/film/feed/"},
-    {"name": "Variety TV", "url": "https://variety.com/v/tv/feed/"},
-    {"name": "Deadline", "url": "https://deadline.com/feed/"},
-    {"name": "Hollywood Reporter", "url": "https://www.hollywoodreporter.com/c/news/feed"},
-    {"name": "Euronews Cinema", "url": "https://www.euronews.com/rss?level=tag&name=cinema"}
-]
