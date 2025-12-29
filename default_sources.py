75f0508851ddcf98c611edcfe9fd54893a620cd0
ieajohsila <50976723+ieajohsila@users.noreply.github.com>
2025-12-29 21:45:59 +0330

    Update default_sources.py

--- default_sources.py
+++ default_sources.py
@@ -14,5 +14,7 @@ DEFAULT_SCRAPE_SITES = [
     "https://www.hollywoodreporter.com/c/news/",
     "https://www.imdb.com/news/top/",
     "https://www.screendaily.com/news",
+    "https://www.zoomg.ir/cinema/feed/",
     "https://www.empireonline.com/movies/news/"
 ]
+
